876996a5a1ffd0ad6217246ddf38aa85bfcb23b2
Ravil <ravilgab@yahoo.com>
2022-11-14 00:49:28 +0300

    Project created

---
+++ Homework_2.28_1_Constraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2.28_1_Constraints
+//
+//  Created by Ravil on 14.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
